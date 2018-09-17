d4f56d5ca58dc12413d12a37e2868d5c0c50f3fe
davide.monticelli <davide.monticelli@hotmail.it>
2018-09-17 23:32:22 +0200

    set test env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-//mongoose.connect('mongodb://montipirlo21:Dasilu.21@ds013559.mlab.com:13559/stark-temple-70258');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
-  mongoose
-}
+    mongoose
+};
+ 
