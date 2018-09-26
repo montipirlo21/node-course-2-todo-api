cb69912e5bf91c7e9e572ae5b3b7f2060bec22e3
davide.monticelli <davide.monticelli@hotmail.it>
2018-09-26 10:58:38 +0200

    update config

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,22 @@
 ﻿var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === 'production') {
-    process.env.MONGODB_URI = 'mongodb://montipirlo21:Dasilu.21@ds013559.mlab.com:13559/stark-temple-70258';
-}
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });    
+
+};
+
+
+//if (env === 'development') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//} else if (env === 'test') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//} else if (env === 'production') {
+//    process.env.MONGODB_URI = 'mongodb://montipirlo21:Dasilu.21@ds013559.mlab.com:13559/stark-temple-70258';
+//}
